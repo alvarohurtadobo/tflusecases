21b8dad15ea92abe24ec1ab57454838655052caf
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-08-14 23:11:43 +0200

    [FEA] Controlled method on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,13 +8,22 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-    let batteryChannel = FlutterMethodChannel(name: "com.example.verygoodcore.tflusecases/native", binaryMessenger: controller.binaryMessenger)
+    // let batteryChannel = FlutterMethodChannel(name: "com.example.verygoodcore.tflusecases/native", binaryMessenger: controller.binaryMessenger)
+    // batteryChannel.setMethodCallHandler {(
+    //   call: FlutterMethodCall, result: @escaping FlutterResult
+    // ) in 
+    // if call.method == "getBatteryLevel" {
+    //   self.getBatteryLevel(result: result)
+    // } else {
+    //   result(FlutterMethodNotImplemented)
+    // }}
+    let channel = FlutterMethodChannel(name: "com.example.verygoodcore.tflusecases/native", binaryMessenger: controller.binaryMessenger)
 
-    batteryChannel.setMethodCallHandler {(
+    channel.setMethodCallHandler {(
       call: FlutterMethodCall, result: @escaping FlutterResult
     ) in 
-    if call.method == "getBatteryLevel" {
-      self.getBatteryLevel(result: result)
+    if call.method == "takePicture" {
+      self.takePicture(result: result)
     } else {
       result(FlutterMethodNotImplemented)
     }}
@@ -33,5 +42,30 @@ import UIKit
       result(
         FlutterError(code: "UNAVAILABLE", message: "Battery level not available", details: nil))
     }
+  }
+
+  private func takePicture(result: @escaping FlutterResult){
+    let  imagePickerController = UIImagePickerController()
+    imagePickerController.sourceType  = .camera
+    imagePickerController.delegate = self 
+
+    if let viewController = window?.rootViewController {
+      viewController.present(imagePickerController, animated: true)
+    }
+  }
+}
+
+extension AppDelegate: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
+    if let imageUrl = info[UIImagePickerController.InfoKey.imageUrl] as? URL {
+      let imagePath = imageUrl.path 
+
+      if let controller = window?.rootViewController as? FlutterViewController {
+        let channel = FlutterMethodChannel(name: "com.example.verygoodcore.tflusecases/native", binaryMessenger: controller.binaryMessenger)
+        channel.invokeMethod("takePicture", arguments: imagePath)
+      }
+    }
+
+    picker.dismiss(animated: true, completion: nil)
   }
 }

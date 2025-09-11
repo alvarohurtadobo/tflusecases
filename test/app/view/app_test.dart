f3eada43d079b8ba2831a3dc59ee6fe5918f12fc
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-09-12 01:13:09 +0200

    [FEA] Removed Bloc to replace for riverpod

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tflusecases/app/app.dart';
-import 'package:tflusecases/counter/counter.dart';
 import 'package:tflusecases/service.dart';
-import 'package:tflusecases/voide/services/speech_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final tfService = TFService();
   await tfService.loadModel();
-  final speechService = SpeechService();
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(
-        App(
-          service: tfService,
-          speechService: speechService,
-        ),
-      );
-      expect(find.byType(CounterPage), findsOneWidget);
-    });
-  });
 }
